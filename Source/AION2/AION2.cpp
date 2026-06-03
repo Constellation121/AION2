6ab76f74721f097f1ee63390a0885d9aaec8f719
jengdeuk <jengdeuk@gmail.com>
2026-06-03 18:45:37 +0900

    Initial commit

---
+++ Source/AION2/AION2.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AION2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AION2, "AION2" );
