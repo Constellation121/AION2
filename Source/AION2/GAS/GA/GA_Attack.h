7a2adc0ec0cc0c9213d5cf11bd186d7fefe52adf
jengdeuk <jengdeuk@gmail.com>
2026-06-23 15:53:46 +0900

    [Feat_GAS] 공격 판정 처리 작업 중

--- Source/AION2/GAS/GA/GA_Attack.h
+++ Source/AION2/GAS/GA/GA_Attack.h
@@ -5,6 +5,39 @@
 #include "Character/Daeva/Daeva.h"
 #include "GA_Attack.generated.h"
 
+USTRUCT(BlueprintType)
+struct FTraceData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly)
+	float Range = 0.0f;
+
+	UPROPERTY(EditDefaultsOnly)
+	float Radius = 0.0f;
+
+	UPROPERTY(EditDefaultsOnly)
+	FVector StartOffset = FVector::ZeroVector;
+
+	UPROPERTY(EditDefaultsOnly)
+	FVector Direction = FVector::ZeroVector;
+};
+
+USTRUCT(BlueprintType)
+struct FAttackData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly)
+	float DamageMultiplier = 1.f;
+
+	//UPROPERTY(EditDefaultsOnly)
+	//FGameplayTag HitGameplayCueTag;
+
+	UPROPERTY(EditDefaultsOnly)
+	FTraceData TraceData;
+};
+
 UCLASS()
 class AION2_API UGA_Attack : public UGameplayAbility
 {
@@ -12,6 +45,17 @@ class AION2_API UGA_Attack : public UGameplayAbility
 
 public:
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+
+protected:
+	UFUNCTION()
+	void OnMontageTaskFinished();
+
+	UFUNCTION()
+	void OnMontageTaskCancelled();
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "AttackData", meta = (AllowPrivateAccess = "true"))
+	FAttackData AttackData;
 
 private:
 	UPROPERTY(EditDefaultsOnly, Category = "Effects", meta = (AllowPrivateAccess = "true"))
@@ -29,11 +73,4 @@ private:
 
 	UPROPERTY(EditDefaultsOnly, Category = "Montage", meta = (AllowPrivateAccess = "true"))
 	FName StartSectionName;
-
-protected:
-	UFUNCTION()
-	void OnMontageTaskFinished();
-
-	UFUNCTION()
-	void OnMontageTaskCancelled();
 };
