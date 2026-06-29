b12b0adfb6ea81d918632cb5a6c0513b197af71a
SuYeon <includeashe@gmail.com>
2026-06-29 17:43:02 +0900

    [Feat_UI] UI 관련 Texture 추가 임포트 및 Mosnter HUD 작업 중

---
+++ Source/AION2/UI/AOMonsterHUDWidget.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/AOUserWidgetBase.h"
+#include "AOMonsterHUDWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AION2_API UAOMonsterHUDWidget : public UAOUserWidgetBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
