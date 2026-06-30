ed66a70ede2793d4f75e34ba1fa18fb7b747c3a9
jengdeuk <jengdeuk@gmail.com>
2026-06-30 14:46:38 +0900

    [Feat_Siliator] AI 세팅 작업 중

---
+++ Source/AION2/AI/Siliator/SiliatorAIController.cpp
@@ -0,0 +1,2 @@
+#include "AI/Siliator/SiliatorAIController.h"
+
---
+++ Source/AION2/AI/Siliator/SiliatorAIController.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AI/AIMonsterControllerBase.h"
+#include "SiliatorAIController.generated.h"
+
+UCLASS()
+class AION2_API ASiliatorAIController : public AAIMonsterControllerBase
+{
+	GENERATED_BODY()
+	
+};
