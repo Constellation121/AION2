56d2ffc0a96b288085892c4de64b834abecd5f18
Kim youngwoo <keiprogramo@gmail.com>
2026-07-03 18:34:35 +0900

    [Feat_Lutalis] Lutalis 보스 커밋
    - State Tree
    - ABP
    - Montage
    - BaseAttack GA
    - GameplayTag 추가

--- Source/AION2/Character/Monster/Boss/Lutalis/Lutalis.cpp
+++ Source/AION2/Character/Monster/Boss/Lutalis/Lutalis.cpp
@@ -2,7 +2,28 @@
 
 
 #include "Character/Monster/Boss/Lutalis/Lutalis.h"
+#include "GAS/AttributeSet/AOAttributeSet.h"
 
+ALutalis::ALutalis(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	DungeonBossIndex = 2;
+}
 
+void ALutalis::BeginPlay()
+{
+	Super::BeginPlay();
 
+	InitAttributeSet();
+}
 
+void ALutalis::InitAttributeSet()
+{
+	Super::InitAttributeSet();
+
+	AttributeSet->InitHealth(5000.f);
+	AttributeSet->InitMaxHealth(5000.f);
+
+	AttributeSet->InitStamina(100.f);
+	AttributeSet->InitMaxStamina(100.f);
+}
